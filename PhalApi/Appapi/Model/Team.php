c160b603cc26b8283a11bac26fd30cb76695e2a8
陈精松 <771084407@qq.com>
2025-12-15 19:27:17 +0800

    提交代码

--- PhalApi/Appapi/Model/Team.php
+++ PhalApi/Appapi/Model/Team.php
@@ -1,10 +1,10 @@
 <?php
 
 class Model_Team extends PhalApi_Model_NotORM {
-	
 
-	/*获取我的团队页面我的基本信息*/
-	public function getMyInfo($uid){
+
+    /*获取我的团队页面我的基本信息*/
+    public function getMyInfo($uid){
         $info=DI()->notorm->user
             ->select("id,user_nicename,avatar,bg_img,avatar_thumb,sex,consumption,votestotal,team_level,team_count,agent_count,team_live_count,team_vip_count")
             ->where('id=?',$uid)
@@ -40,10 +40,10 @@ class Model_Team extends PhalApi_Model_NotORM {
         }
 
         return $info;
-	}
+    }
 
-	/*获取直推列表*/
-	public function getMyTeamLists($uid,$p,$isauth,$sort,$key){
+    /*获取直推列表*/
+    public function getMyTeamLists($uid,$p,$isauth,$sort,$key){
         if($p<1){
             $p=1;
         }
@@ -59,7 +59,7 @@ class Model_Team extends PhalApi_Model_NotORM {
         if(empty($sort)){
             $sort = 'addtime DESC';
         }
-        $sql = 'SELECT a.one_uid AS ouid,a.addtime,u.id,u.isauth,u.user_nicename,u.user_login,u.avatar,u.bg_img,u.avatar_thumb,u.sex,u.consumption,u.votestotal,u.team_level,u.team_count,u.agent_count,u.team_live_count,u.team_vip_count '
+        $sql = 'SELECT a.one_uid AS ouid,a.addtime,u.id,u.isauth,u.user_nicename,u.islive,u.user_login,u.avatar,u.bg_img,u.avatar_thumb,u.sex,u.consumption,u.votestotal,u.team_level,u.team_count,u.agent_count,u.team_live_count,u.team_vip_count '
             . 'FROM cmf_agent AS a INNER JOIN cmf_user AS u '
             . 'ON a.uid = u.id '
             . 'WHERE a.one_uid = '.$uid.' '.$where
@@ -69,16 +69,40 @@ class Model_Team extends PhalApi_Model_NotORM {
         if($list){
             foreach($list as $k=>$v){
                 $list[$k]['level']=getLevel($v['consumption']);
-                $list[$k]['level_anchor']=getLevelAnchor($v['votestotal']);
+                $level = 1;
+                for ($i = 1; $i <= 10; $i++) {
+                    if ($list[$k]['level'] <= 10 * $i) {
+                        $level = $i;
+                        break;
+                    }
+                }
+                if ($v['consumption'] < 1000) {
+                    $list[$k]['level'] = 0;
+                }
+                $list[$k]['level_thumb'] = get_upload_path('/images/new_level/level_' . $level . '@2x.png');
+                $list[$k]['level_bg_thumb'] = get_upload_path('/images/new_level/level_bg_' . $level . '@2x.png');
+                if ($v['islive'] == 1 && $v['votestotal'] > 0) {
+                    $list[$k]['level_anchor'] = getLevelAnchor($v['votestotal']);
+                    $list[$k]['level_anchor_thumb'] = get_upload_path('/images/new_level/level_anchor_' . $v['level_anchor'] . '@3x.png');
+                } else {
+                    $list[$k]['level_anchor'] = 0;
+                    $list[$k]['level_anchor_thumb'] = get_upload_path('/images/new_level/level_anchor_1@3x.png');
+                }
                 $list[$k]['level_team']=$v['team_level'];
+                if ($v['level_team'] > 0) {
+                    $list[$k]['level_team_thumb'] = get_upload_path('/images/new_level/level_team_' . $v['level_team'] . '@3x.png');
+                } else {
+                    $list[$k]['level_team_thumb'] = get_upload_path('/images/new_level/level_team_1@3x.png');
+                }
                 $list[$k]['vip']=getUserVip($v['id']);
                 $list[$k]['addtime']=date('Y-m-d',$v['addtime']);
                 $list[$k]['avatar']=get_upload_path($v['avatar']);
                 $list[$k]['avatar_thumb']=get_upload_path($v['avatar_thumb']);
+                unset($list[$k]['team_level']);
             }
         }
         return $list;
 
-	}
-	
+    }
+
 }
