6e47ed47a3459372e0a25c4adc5f530d02c978fa
陈精松 <771084407@qq.com>
2025-12-16 11:17:32 +0800

    提交代码

--- app/admin/controller/AdminpostController.php
+++ app/admin/controller/AdminpostController.php
@@ -222,17 +222,25 @@ class AdminpostController extends AdminbaseController {
             $data['user_nicename']=$userinfo['user_nicename'];
             $data['addtime']=time();
             // 修改多国语言
-            $language = $data['language'];
-            $language_content = $data['language_content'];
-            unset($data['language']);
-            unset($data['language_content']);
+            if(!empty($data['language_content'])){
+                $language_content = $data['language_content'];
+                unset($data['language_content']);
+            }
+            if(!empty($data['language'])){
+                $language = $data['language'];
+                unset($data['language']);
+            }
 
             $rs = DB::name('admin_post')->update($data);
             if(!$id){
                 $this->error("修改失败！");
             }
-            update_language_translate('admin_post', 'title', $id, $language);
-            update_language_translate('admin_post', 'content', $id, $language_content);
+            if(!empty($data['language_content'])){
+                update_language_translate('admin_post', 'content', $id, $language_content);
+            }
+            if(!empty($data['language'])){
+                update_language_translate('admin_post', 'title', $id, $language);
+            }
             $action="修改文章：{$id}";
             setAdminLog($action);
 
