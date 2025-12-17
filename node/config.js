ef52c2bf7bcee61c9d93753d226da5e48a9b0699
pksslookout <642405843@qq.com>
2025-12-17 16:24:20 +0800

    Delete node/config.js

--- node/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	'REDISHOST' : '127.0.0.1',
-	'REDISPASS' : '27199673',
-	'REDISPORT' : '6379',
-	'TOKEN'		: '1234567',
-	'sign_key'	: '76576076c1f5f657b634e966c8836a06',
-	'socket_port': '19967',
-	'WEBADDRESS': 'https://test.popolive.net/appapi/'
-}
