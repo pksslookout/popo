d0f0913927b99691576446139cebccbeeaadbb31
陈精松 <771084407@qq.com>
2025-12-18 13:45:29 +0800

    提交代码

--- app/portal/controller/IndexController.php
+++ app/portal/controller/IndexController.php
@@ -10,14 +10,43 @@
 // +----------------------------------------------------------------------
 namespace app\portal\controller;
 
-use cmf\controller\HomeBaseController;
+use think\Controller;
+use think\facade\View;
 
-class IndexController extends HomeBaseController
+class IndexController extends Controller
 {
 
     // 首页
     public function index()
     {
+
+        /* redis缓存开启 */
+        connectionRedis();
+
+        $siteInfo = getConfigPub();
+
+        if(isset($siteInfo['sina_icon'])){
+            $siteInfo['sina_icon']=get_upload_path($siteInfo['sina_icon']);
+        }
+        if(isset($siteInfo['qq_icon'])){
+            $siteInfo['qq_icon']=get_upload_path($siteInfo['qq_icon']);
+        }
+        if(isset($siteInfo['apk_ewm'])){
+            $siteInfo['apk_ewm']=get_upload_path($siteInfo['apk_ewm']);
+        }
+        if(isset($siteInfo['ipa_ewm'])){
+            $siteInfo['ipa_ewm']=get_upload_path($siteInfo['ipa_ewm']);
+        }
+        if(isset($siteInfo['wechat_ewm'])){
+            $siteInfo['wechat_ewm']=get_upload_path($siteInfo['wechat_ewm']);
+        }
+        if(isset($siteInfo['qr_url'])){
+            $siteInfo['qr_url']=get_upload_path($siteInfo['qr_url']);
+        }else{
+            $siteInfo['qr_url']='';
+        }
+
+        View::share('site_info', $siteInfo);
         return $this->fetch();
     }
     
