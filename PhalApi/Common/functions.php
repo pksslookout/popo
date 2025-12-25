dd97950c4cbba63641be77be5e165bf69dcf57c9
陈精松 <771084407@qq.com>
2025-12-25 16:56:17 +0800

    提交代码

--- PhalApi/Common/functions.php
+++ PhalApi/Common/functions.php
@@ -1228,18 +1228,8 @@ function connectionRedis(){
 
 	/* K */
 	function NumberFormatT($num){
-		if($num<1000){
-
-		}else if($num<10000){
-			$num=round($num/1000,2).T('K');
-		}else if($num<1000000){
-			$num=round($num/10000,2).T('W');
-		}else if($num<100000000){
-			$num=round($num/10000,1).T('W');
-		}else if($num<10000000000){
-			$num=round($num/100000000,2).T('B');
-		}else{
-			$num=round($num/100000000,1).T('B');
+		if($num>1000){
+			$num=round($num/1000,2).T('k');
 		}
 		return (string)$num;
 	}
